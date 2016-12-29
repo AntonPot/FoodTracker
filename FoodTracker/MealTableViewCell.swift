303fcc3e6502461cdc9e86f4d22cc7ead2841a14
Marko Anton Potocnik <markoapotocnik@hotmail.com>
2016-12-29 13:39:02 +0100

    Checkpoint: Prototype cell added

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Marko Anton Potocnik on 29/12/2016.
+//  Copyright © 2016 Marko Anton Potocnik. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
